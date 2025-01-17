d5045a22c0f990426a7ee4511736d58b98b94381
lkrenz <liamkrenz100@gmail.com>
2025-01-16 19:48:31 -0800

    Working on iterative approach to solve stack overflow.

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * The class WeatherPatterns finds the longest span of days in which
  * each day’s temperature is higher than on the previous day in that sequence.
@@ -40,6 +45,49 @@ public class WeatherPatterns {
         }
 
         return runMax;
+    }
+
+    public static int naiveHelper(int[] temperatures) {
+
+        // Queue order goes index, maxTemp, streak
+        Queue<int[]> data = new LinkedList<>();
+
+        ArrayList<Integer> streaks = new ArrayList<>();
+        int[] initial = new int[3];
+        initial[1] = -9999;
+        data.add(initial);
+
+
+        while (!data.isEmpty()) {
+            int[] current = data.poll();
+            int index = current[0];
+            int streak = current[2];
+            int max = current[1];
+            if (index >= temperatures.length) {
+                streaks.add(streak);
+                continue;
+            }
+            if (temperatures[index] > max) {
+                initial = new int[3];
+                initial[0] = index + 1;
+                initial[1] = temperatures[index];
+                initial[2] = streak + 1;
+                data.add(initial);
+
+                initial = new int[3];
+                initial[0] = index + 1;
+                initial[1] = max;
+                initial[2] = streak;
+                data.add(initial);
+            }
+            else {
+                initial = new int[3];
+                initial[0] = index + 1;
+                initial[1] = max;
+                initial[2] = streak;
+                data.add(initial);
+            }
+        }
     }
 
 }
