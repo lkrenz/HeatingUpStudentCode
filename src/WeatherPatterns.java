17ede23d58473771d6b84368b8e71057bbbdfeca
lkrenz <liamkrenz100@gmail.com>
2025-01-16 19:00:15 -0800

    Started naive solution

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -19,4 +19,28 @@ public class WeatherPatterns {
 
         return 0;
     }
+
+    public int naiveApproach(int[] temperatures) {
+        return naiveHelper(0, temperatures, -9999, 0);
+    }
+
+    public int naiveHelper(int index, int[] temperatures, int max, int streak) {
+        if (index >= temperatures.length) {
+            return max;
+        }
+        int runMax = 0;
+        if (temperatures[index] > max) {
+            runMax = naiveHelper(index + 1, temperatures, temperatures[index], streak + 1);
+            int temp = naiveHelper(index + 1, temperatures, max, streak);
+            if (temp > runMax) {
+                runMax = temp;
+            }
+        }
+        else {
+            runMax = naiveHelper(index + 1, temperatures, max, streak);
+        }
+
+        return runMax;
+    }
+
 }
